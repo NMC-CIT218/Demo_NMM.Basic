c3778d1fc208f591ac93c0073499461cb8d84aaf
John Velis <john@johnvelis.com>
2016-01-24 16:49:20 -0500

    commit449

--- Demo_NMM.Basic.D1/Controllers/BreweriesController.cs
+++ Demo_NMM.Basic.D1/Controllers/BreweriesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo_NMM.Basic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,46 @@ namespace Demo_NMM.Basic.Controllers
         // GET: Breweries
         public ActionResult Index()
         {
-            return View();
+            List<Brewery> breweries = InitializeBreweries();
+
+            return View(breweries);
+        }
+
+        private List<Brewery> InitializeBreweries()
+        {
+            List<Brewery> breweries = new List<Brewery>();
+
+            breweries.Add(new Brewery
+            {
+                Name = "Right Brain Brewery",
+                Address = "225 E. 16th St",
+                City = "Traverse City",
+                State = AppEnum.StateAbrv.MI,
+                Zip = "49684",
+                Phone = "(231) 944-1239"
+            });
+
+            breweries.Add(new Brewery
+            {
+                Name = "Acoustic Tap Room",
+                Address = "119 Maple St",
+                City = "Traverse City",
+                State = AppEnum.StateAbrv.MI,
+                Zip = "49684",
+                Phone = "(231) 883-2012"
+            });
+
+            breweries.Add(new Brewery
+            {
+                Name = "Beggars Brewery",
+                Address = "4177 Village Park Dr. Suite C",
+                City = "Traverse City",
+                State = AppEnum.StateAbrv.MI,
+                Zip = "49684",
+                Phone = "N/A"
+            });
+
+            return breweries;
         }
     }
 }
