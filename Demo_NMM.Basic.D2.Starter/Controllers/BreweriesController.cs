33a30235c909d6ed1c65c8d9b17e64fc028960db
John Velis <john@johnvelis.com>
2016-02-13 10:11:53 -0500

    commit1011

---
+++ Demo_NMM.Basic.D2.Starter/Controllers/BreweriesController.cs
@@ -0,0 +1,89 @@
+﻿using Demo_NMM.Basic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_NMM.Basic.Controllers
+{
+    public class BreweriesController : Controller
+    {
+        // GET: Breweries
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowTable()
+        {
+            List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
+
+            Session["Breweries"] = breweries;
+
+            return View(breweries);
+        }
+
+        public ActionResult ShowList()
+        {
+            List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
+
+            Session["Breweries"] = breweries;
+
+            return View(breweries);
+        }
+
+        public ActionResult ShowDetail(int id)
+        {
+            return View();
+        }
+
+
+        public ActionResult DeleteBrewery(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteBrewery(FormCollection form)
+        {
+            return View();
+        }
+
+        public ActionResult CreateBrewery()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateBrewery(FormCollection form)
+        {
+            return View();
+        }
+
+        public ActionResult UpdateBrewery(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateBrewery(FormCollection form)
+        {
+            return View();
+        }
+
+        public ActionResult ReloadData()
+        {
+            DAL.Data.InitializeBreweries();
+
+            return Redirect("/Breweries/ShowTable");
+        }
+
+        private int GetNextID()
+        {
+            List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
+
+            return breweries.Max(x => x.ID) + 1;
+        }
+    }
+}
--- Demo_NMM.Basic.D2/Controllers/BreweriesController.cs
+++ Demo_NMM.Basic.D2/Controllers/BreweriesController.cs
@@ -12,12 +12,6 @@ namespace Demo_NMM.Basic.Controllers
         // GET: Breweries
         public ActionResult Index()
         {
-            if (Session["ActiveBrewerySession"] == null)
-            {
-                Session["Breweries"] = InitialBreweries();
-                Session["ActiveBrewerySession"] = true;
-            }
-
             return View();
         }
 
@@ -158,7 +152,7 @@ namespace Demo_NMM.Basic.Controllers
 
         public ActionResult ReloadData()
         {
-            Session["Breweries"] = InitialBreweries();
+            DAL.Data.InitializeBreweries();
 
             return Redirect("/Breweries/ShowTable");
         }
@@ -168,46 +162,6 @@ namespace Demo_NMM.Basic.Controllers
             List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
             
             return breweries.Max(x => x.ID) + 1;
-        }
-
-        private List<Brewery> InitialBreweries()
-        {
-            List<Brewery> breweries = new List<Brewery>();
-
-            breweries.Add(new Brewery
-            {
-                ID = 1,
-                Name = "Right Brain Brewery",
-                Address = "225 E. 16th St",
-                City = "Traverse City",
-                State = AppEnum.StateAbrv.MI,
-                Zip = "49684",
-                Phone = "(231) 944-1239"
-            });
-
-            breweries.Add(new Brewery
-            {
-                ID = 2,
-                Name = "Acoustic Tap Room",
-                Address = "119 Maple St",
-                City = "Traverse City",
-                State = AppEnum.StateAbrv.MI,
-                Zip = "49684",
-                Phone = "(231) 883-2012"
-            });
-
-            breweries.Add(new Brewery
-            {
-                ID = 3,
-                Name = "Beggars Brewery",
-                Address = "4177 Village Park Dr. Suite C",
-                City = "Traverse City",
-                State = AppEnum.StateAbrv.MI,
-                Zip = "49684",
-                Phone = "N/A"
-            });
-
-            return breweries;
         }
     }
 }
