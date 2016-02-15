6f46f2aeba03bf2ecfedb3bceb8ab239244bcd33
John Velis <john@johnvelis.com>
2016-02-15 07:32:53 -0500

    commit725

--- Demo_NMM.Basic.D2.Starter/Controllers/BreweriesController.cs
+++ Demo_NMM.Basic.D2.Starter/Controllers/BreweriesController.cs
@@ -9,12 +9,17 @@ namespace Demo_NMM.Basic.Controllers
 {
     public class BreweriesController : Controller
     {
-        // GET: Breweries
+        // 
+        // Landing page
+        //
         public ActionResult Index()
         {
             return View();
         }
 
+        //
+        // Show table view
+        //
         public ActionResult ShowTable()
         {
             List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
@@ -24,6 +29,9 @@ namespace Demo_NMM.Basic.Controllers
             return View(breweries);
         }
 
+        //
+        // Show list view
+        //
         public ActionResult ShowList()
         {
             List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
@@ -33,45 +41,68 @@ namespace Demo_NMM.Basic.Controllers
             return View(breweries);
         }
 
+        //
+        // Show detail view
+        //
         public ActionResult ShowDetail(int id)
         {
             return View();
         }
 
-
+        //
+        // Show brewery to delete in detail view
+        // 
         public ActionResult DeleteBrewery(int id)
         {
             return View();
         }
 
+        //
+        // Process request from delete brewery form
+        //
         [HttpPost]
         public ActionResult DeleteBrewery(FormCollection form)
         {
             return View();
         }
 
+        //
+        // Show new brewery form
+        //
         public ActionResult CreateBrewery()
         {
             return View();
         }
 
+        //
+        // Process request from new brewery form
+        //
         [HttpPost]
         public ActionResult CreateBrewery(FormCollection form)
         {
             return View();
         }
 
+        //
+        // Show brewery to update in edit view
+        //
         public ActionResult UpdateBrewery(int id)
         {
             return View();
         }
 
+        //
+        // Process request from update brewery form
+        //
         [HttpPost]
         public ActionResult UpdateBrewery(FormCollection form)
         {
             return View();
         }
 
+        //
+        // Reload initial data set
+        //
         public ActionResult ReloadData()
         {
             DAL.Data.InitializeBreweries();
@@ -79,6 +110,9 @@ namespace Demo_NMM.Basic.Controllers
             return Redirect("/Breweries/ShowTable");
         }
 
+        //
+        // Get next ID number ensure uniqueness
+        //
         private int GetNextID()
         {
             List<Brewery> breweries = (List<Brewery>)Session["Breweries"];
